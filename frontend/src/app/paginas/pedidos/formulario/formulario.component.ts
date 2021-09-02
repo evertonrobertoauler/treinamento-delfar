7b88a4703f9e0b3771e3d146aafb899cd7607e10
Samuel Machado dos Santos <machado.samuel2018@gmail.com>
2021-09-02 09:48:11 -0300

    removendo imports não utilizados

--- frontend/src/app/paginas/pedidos/formulario/formulario.component.ts
+++ frontend/src/app/paginas/pedidos/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { toPairs } from 'lodash-es';
 import { tap } from 'rxjs/operators';
